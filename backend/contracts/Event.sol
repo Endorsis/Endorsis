5a1121debea1caa6dab311d4cc206b2b866cdce3
Bertan Köfön <kofon@Bertan-Air.home>
2024-04-12 22:42:48 +0200

    added basic NFT logic

--- backend/contracts/Event.sol
+++ backend/contracts/Event.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Event {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Event is ERC721Enumerable, Ownable {
     string public eventName;
     string public eventDescription;
-    uint8 public startDate;
-    uint8 public endDate;
+    uint256 public startDate;
+    uint256 public endDate;
 
     struct User {
         string userName;
@@ -13,26 +16,33 @@ contract Event {
     }
 
     mapping(address => User) public userRecords;
+    mapping(uint256 => string) private _tokenURIs;  // Mapping for token URIs
 
     error UserAlreadyExists();
     error EventNotStarted();
     error DeadlineExpired();
 
-    constructor(string memory _eventName, string memory _eventDescription, uint8 memory _startDate, uint8 memory _endDate, string memory _userName) {
-        eventName = _eventName
-        eventDescription = _eventDescription
-        startDate = _startDate
-        endDate = _endDate
+    constructor(
+        string memory _eventName,
+        string memory _eventDescription,
+        uint256 _startDate,
+        uint256 _endDate,
+        string memory _userName
+    ) ERC721("EventToken", "EVT") {
+        eventName = _eventName;
+        eventDescription = _eventDescription;
+        startDate = _startDate;
+        endDate = _endDate;
     }
 
     function joinEvent(string memory userName) external {
         if (block.timestamp > endDate) {
             revert DeadlineExpired();
         }
-        if (userRecords[msg.sender].userName != "") {
+        if (bytes(userRecords[msg.sender].userName).length != 0) {
             revert UserAlreadyExists();
         }
-        User memory newUser = User(userName, []);
+        User memory newUser = User(userName, new string );
         userRecords[msg.sender] = newUser;
     }
 
@@ -46,8 +56,19 @@ contract Event {
         userRecords[msg.sender].endorsements.push(endorsement);
     }
 
-    function deadline() external {
-        // mint NFT
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = totalSupply() + 1;
+        _safeMint(recipient, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
 }

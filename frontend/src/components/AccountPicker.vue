4a1207cb7a6cc0da587dcb4939f077e9cff5f2fe
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-04-12 22:52:26 +0200

    working on connect wallet

--- frontend/src/components/AccountPicker.vue
+++ frontend/src/components/AccountPicker.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue';
-
 import { Network, networkName, useEthereumStore } from '../stores/ethereum';
 import JazzIcon from './JazzIcon.vue';
 import { abbrAddr } from '@/utils/utils';
 import { useMedia } from '@/utils/useMediaQuery';
 import { MetaMaskNotInstalledError } from '@/utils/errors';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 const eth = useEthereumStore();
-
 const netName = computed(() => networkName(eth.network));
 const unkNet = computed(() => eth.network === Network.Unknown);
-
 const connecting = ref(false);
-const isMetaMaskInstalled = ref(false);
+const isMetaMaskInstalled = ref(true); // assume installed until proven otherwise
 
 async function connectWallet() {
   if (!isMetaMaskInstalled.value) {
@@ -27,10 +25,10 @@ async function connectWallet() {
       await window.ethereum.request({ method: 'eth_requestAccounts' });
     }
   } catch (err) {
-    if (!(err instanceof MetaMaskNotInstalledError)) {
-      throw err;
-    } else {
+    if (err instanceof MetaMaskNotInstalledError) {
       isMetaMaskInstalled.value = false;
+    } else {
+      console.error("Connection failed:", err);
     }
   } finally {
     connecting.value = false;
@@ -40,18 +38,21 @@ async function connectWallet() {
 const isXlScreen = useMedia('(min-width: 1280px)');
 
 onMounted(async () => {
-  try {
-    await eth.getEthereumProvider();
-  } catch (err) {
-    if (!(err instanceof MetaMaskNotInstalledError)) {
-      throw err;
-    } else {
-      isMetaMaskInstalled.value = false;
+  const ethProvider = await detectEthereumProvider();
+if (ethProvider && ethProvider === (window.ethereum as any)) {
+  console.log("MetaMask is installed.");
+  // isMetaMaskInstalled.value = true;
+    try {
+      await eth.getEthereumProvider();
+    } catch (err) {
+      console.error("Failed to get Ethereum provider:", err);
     }
-  } finally {
-    isMetaMaskInstalled.value = true;
+  } else {
+    console.log("MetaMask is not installed.");
+    isMetaMaskInstalled.value = false;
   }
 });
+
 </script>
 
 <template>
@@ -63,22 +64,16 @@ onMounted(async () => {
     <span class="account-picker-content" v-if="!connecting && eth.address">
       <JazzIcon :size="isXlScreen ? 60 : 30" :address="eth.address" />
       <span class="font-mono font-bold">
-        <abbr :title="eth.address" class="block no-underline">{{ abbrAddr(eth.address) }}</abbr>
-        <span v-if="isXlScreen" class="font-normal" :class="{ 'unk-net': unkNet }">{{
-          netName
-        }}</span>
+        <abbr :title="eth.address">{{ abbrAddr(eth.address) }}</abbr>
+        <span v-if="isXlScreen" class="font-normal" :class="{ 'unk-net': unkNet }">{{ netName }}</span>
       </span>
     </span>
     <span class="account-picker-content" v-else-if="!isMetaMaskInstalled">
-      <span>
-        <span>Install MetaMask</span>
-      </span>
+      <span>Install MetaMask</span>
     </span>
     <span class="account-picker-content" v-else>
-      <span>
-        <span v-if="connecting">Connecting…</span>
-        <span v-else>Connect Wallet</span>
-      </span>
+      <span v-if="connecting">Connecting…</span>
+      <span v-else>Connect Wallet</span>
     </span>
   </button>
 </template>

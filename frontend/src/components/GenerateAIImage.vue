334809fed4e2841a3d2fd98b009aa39801dbfa57
defuera <justdeveloper@gmail.com>
2024-04-13 15:14:11 +0200

    Add API call to generate AI image

--- frontend/src/components/GenerateAIImage.vue
+++ frontend/src/components/GenerateAIImage.vue
@@ -1,21 +1,13 @@
 <template>
   <form @submit.prevent="generateAIImage(inputText)">
-    <div class="form-group">
-      <input
-        type="text"
-        id="aiImageText"
-        class="peer"
-        placeholder=" "
-        v-model="inputText"
-        required
-      />
-      <label
-        for="aiImageText"
-        class="peer-focus:text-primaryDark peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5"
-      >
+    <h2 class="text-2xl font-bold mb-4 text-white">Generate AI Image</h2>
+    <div class="form-group py-4">
+      <label for="aiImageText"
+        class="block text-white peer-focus:text-primaryDark peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5">
         Enter text for AI image:
         <span class="text-red-500">*</span>
       </label>
+      <input type="text" id="aiImageText" class="peer" placeholder=" " v-model="inputText" required />
     </div>
 
     <AppButton type="submit" variant="primary" :disabled="isLoading">
@@ -47,13 +39,17 @@ async function generateAIImage(text: string) {
   isLoading.value = true;
 
   try {
-    // Call your AI image generation API here
-    // For example:
-    // const response = await fetch(`/api/generate-image?text=${encodeURIComponent(text)}`);
-    // image.value = await response.json();
+    console.log(`Generating AI image with prompt: ${text}`);
+    const response = await fetch(`https://sticked-api-hwruz2623a-lz.a.run.app/ai/generateImage?prompt=${encodeURIComponent(text)}`);
+    console.log(`Response: ${response}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    image.value = data.image_url; // Extract the image URL from the response
   } catch (error) {
     console.error(error);
-    // errors.value.push(error.message);
+    errors.value.push(error.message);
   } finally {
     isLoading.value = false;
   }

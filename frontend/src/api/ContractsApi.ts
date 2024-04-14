312a853d2dfb176713a661f0153aa17331b84728
Tanguy VANSNICK <191451@umons.ac.be>
2024-04-14 08:56:40 +0200

    generate smart contract id

--- frontend/src/api/ContractsApi.ts
+++ frontend/src/api/ContractsApi.ts
@@ -1,5 +1,6 @@
 import { EndorseeModel } from "@/models/EndorseeModel";
 import { EventModel } from "@/models/EventModel";
+import CryptoJS from 'crypto-js';
 
 export default class ContractsApi {
   private static events: Map<string, EventModel> = new Map();
@@ -11,7 +12,7 @@ export default class ContractsApi {
 
 
     const mockEndorsee2 = new EndorseeModel('Not completed', '0xMockAddress');
-    const mockEvent2 = new EventModel('95ccae2f78868f3e2a4bd22e796701461f993d22373068bf27edee61d50aa3d2', 'Event Uncompleted', 'This is a mock event', [mockEndorsee2], '2022-01-01', '2024-04-14', 'mockPassword');
+    const mockEvent2 = new EventModel('95ccae2f78868f3e2a4bd22e796701461f993d22373068bf27edee61d50aa3d2', 'Event Uncompleted', 'This is a mock event', [mockEndorsee2], '2022-01-01', '2024-04-15', 'mockPassword');
     ContractsApi.events.set(mockEvent2.id, mockEvent2);
   }
 
@@ -36,10 +37,14 @@ export default class ContractsApi {
   }
 
   async createEvent(name: string, description: string, endorseees: EndorseeModel[], startDate: string, endDate: string, password: string) {
-    const event = new EventModel('id', name, description, endorseees, startDate, endDate, password);
+    // generating the id of the contract 
+    const eventInfo = `${name}${description}${JSON.stringify(endorseees)}${startDate}${endDate}${password}`;
+    const sha256Hash = CryptoJS.SHA256(eventInfo).toString();
+    console.log(`SHA256 Event Address: ${sha256Hash}`);
+
+    const event = new EventModel(sha256Hash, name, description, endorseees, startDate, endDate, password);
     ContractsApi.events.set(event.id, event);
 
     console.log(`events: ${ContractsApi.events}`);
   }
-
 }

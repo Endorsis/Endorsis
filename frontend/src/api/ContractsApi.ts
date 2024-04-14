f4e4873ae5d9d437920bd26c2c70003352887bb3
defuera <justdeveloper@gmail.com>
2024-04-14 02:00:24 +0200

    Fix display event detailed

--- frontend/src/api/ContractsApi.ts
+++ frontend/src/api/ContractsApi.ts
@@ -6,16 +6,21 @@ export default class ContractsApi {
 
   constructor() {
     const mockEndorsee = new EndorseeModel('Mock Endorsee', '0xMockAddress');
-    const mockEvent = new EventModel('mockId', 'Mock Event', 'This is a mock event', [mockEndorsee], '2022-01-01', '2022-12-31', 'mockPassword');
+    const mockEvent = new EventModel('95ccae2f78868f3e2a4bd22e796701461f993d22373068bf27edee61d50aa3de', 'Event Completed', 'This is a mock event', [mockEndorsee], '2022-01-01', '2022-12-31', 'mockPassword');
     ContractsApi.events.set(mockEvent.id, mockEvent);
+
+
+    const mockEndorsee2 = new EndorseeModel('Not completed', '0xMockAddress');
+    const mockEvent2 = new EventModel('95ccae2f78868f3e2a4bd22e796701461f993d22373068bf27edee61d50aa3d2', 'Event Uncompleted', 'This is a mock event', [mockEndorsee2], '2022-01-01', '2024-04-14', 'mockPassword');
+    ContractsApi.events.set(mockEvent2.id, mockEvent2);
   }
 
   async submitFeedback(feedback: string, password: string) {
-    // Implement your logic to claim feedback
+    console.log(`submitFeedback ${feedback}, ${password}`);
   }
 
   async claimFeedback(password: string) {
-    // Implement your logic to claim feedback
+    console.log(`claimFeedback password: ${password}`);
   }
 
   async getEvents() {
